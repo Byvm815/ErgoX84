a5e1916c0cef20b05d69d9ae8e1f10bfc71bf43f
lqlklu <lqlklu@qq.com>
2024-11-11 13:15:33 +0000

    use ModifierTapHold

--- firmware/ergox_rs/src/keymap.rs
+++ firmware/ergox_rs/src/keymap.rs
@@ -1,5 +1,5 @@
 use rmk::action::{KeyAction, Action};
-use rmk::keycode::KeyCode;
+use rmk::keycode::{KeyCode, ModifierCombination};
 use rmk::{a, k, layer, mo};
 
 pub(crate) const COL: usize = 7;
@@ -8,7 +8,7 @@ pub(crate) const NUM_LAYER: usize = 3;
 
 macro_rules! mt {
     ($a: ident, $b: ident) => {
-        KeyAction::TapHold(Action::Key(KeyCode::$a), Action::Key(KeyCode::$b))
+        KeyAction::ModifierTapHold(Action::Key(KeyCode::$a), Action::Key($b))
     };
 }
 
@@ -20,7 +20,8 @@ pub fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
             [k!(Escape), k!(F1), k!(F2), k!(F3), k!(F4), k!(F5), k!(F6)],
             [k!(Grave), k!(Kc1), k!(Kc2), k!(Kc3), k!(Kc4), k!(Kc5), a!(No)],
             [k!(Tab), k!(Q), k!(W), k!(E), k!(R), k!(T), a!(No)],
-            [mt!(Escape, LCtrl), k!(A), k!(S), k!(D), k!(F), k!(G), a!(No)],
+            [KeyAction::ModifierTapHold(Action::Key(KeyCode::Escape), ModifierCombination::new_from(false,false,false,false,true)),
+                 k!(A), k!(S), k!(D), k!(F), k!(G), a!(No)],
             [k!(LShift), k!(Z), k!(X), k!(C), k!(V), k!(B), a!(No)],
             [k!(LGui), k!(Minus), k!(Equal), k!(Quote), mo!(1), k!(Space), k!(Escape)],
             // Right
