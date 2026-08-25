c112e807e97f1e9a0e9a30dd8a837e06b9eac317
Byvm815 <2529025686@qq.com>
2026-08-25 23:52:52 +0800

    Update RMK to V0.9

--- firmware/ergox_conf/shell.nix
+++ firmware/ergox_conf/shell.nix
@@ -1,45 +1,49 @@
-##you have to download arrterian.nix-env-selector
-##and build with nix-shell
-
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell rec {
     buildInputs = with pkgs; [
       rustup
-      cargo
       clang
       llvmPackages.bintools
-
       flip-link
       elf2uf2-rs
+      probe-rs-tools
     ];
 
-  RUSTC_VERSION = "latest";
-  #RUSTC_VERSION = "nightly";
-  
-  shellHook = ''
-    export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-    export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-  '';
-
-  ##Add precompiled library to rustc search path
-  RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
-    ##add libraries here (e.g. pkgs.libvmi)
-
-  ]);
-
-  ##Add glibc, clang, glib and other headers to bindgen search path
-  BINDGEN_EXTRA_CLANG_ARGS = (
-    ##Includes with normal include path
-    builtins.map (a: ''-I"${a}/include"'') [
-      pkgs.glibc.dev ##add dev libraries here (e.g. pkgs.libvmi.dev)
-    ]
-  )  ++ [
-  ##Includes with special directory paths
-  ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-  ''-I"${pkgs.glib.dev}/include/glib"''
-  ''-I${pkgs.glib.out}/lib/glib/include/''
-  ];
-
-  ##https://github.com/rust-lang/rust-bindgen#environment-variables
-  LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-}
+    RUSTC_VERSION = "stable";
+    
+    shellHook = ''
+      # 初始化 rustup
+      export RUSTUP_HOME=''${RUSTUP_HOME:-$HOME/.rustup}
+      export CARGO_HOME=''${CARGO_HOME:-$HOME/.cargo}
+      
+      # 只使用 rustup 管理的 cargo/rustc
+      unset CARGO
+      unset RUSTC
+      
+      # 安装工具链（如果没装）
+      rustup toolchain install $RUSTC_VERSION 2>/dev/null || true
+      rustup default $RUSTC_VERSION
+      
+      # 安装 target
+      rustup target add thumbv6m-none-eabi --toolchain $RUSTC_VERSION
+      
+      # 确保 PATH 优先使用 rustup
+      export PATH="$CARGO_HOME/bin:$RUSTUP_HOME/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin:$PATH"
+      
+      export KEYBOARD_TOML_PATH="$PWD/keyboard.toml"
+      
+      echo "Using cargo: $(which cargo)"
+      echo "Using rustc: $(which rustc)"
+      cargo --version
+      rustc --version
+      rustup target list --installed
+    '';
+    
+    BINDGEN_EXTRA_CLANG_ARGS = (builtins.map (a: ''-I"${a}/include"'') [
+      pkgs.glibc.dev
+    ]) ++ [
+      ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+    ];
+    
+    LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+  }
